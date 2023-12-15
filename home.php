77aed2689f85839bb89b07b4370881e74605b342
Prem Oraon <91319598+pink-0xxion@users.noreply.github.com>
2023-12-15 22:43:08 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html >
+<html>
+<head>
+    <title>HOME PAGE</title>
+	<link rel="stylesheet" href="./css/viewStyle.css" />
+</head>
+<body>
+    <h1> BOOK MANAGEMENT SYSTEM </h1>
+	<a href="insertionForm.php">Insert Book</a><br/>
+	<a href="view.php">View Book Records</a><br/>
+	<a href="deleteForm.php">Delete Book Records</a><br/>
+	<a href="updateForm.php">Update Book Records</a><br/>
+</body>
+</html>
---
+++ insertionForm.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html >
+<html>
+<head>
+    <title>Insertion Form</title>
+</head>
+<body>
+    <h1> BOOK MANAGEMENT SYSTEM </h1>
+	
+	<form action="insertion.php" method="post" >
+	 <table>
+	 <tr>
+	 <th>Title</th>
+	 <td><input type="text" name="title" required ></td>
+	 </tr>
+	 <tr>
+	 <th>Price</th>
+	 <td><input type="text" name="price" required ></td>
+	 </tr>
+	 <tr>
+	 <th>Author</th>
+	 <td><input type="text" name="author" required ></td>
+	 </tr>
+	 <tr>
+	 <td><input type="submit" value="insert" ></td>
+	 </tr>
+	 </table>
+	 
+	 
+	 
+	 
+	</form>
+
+</body>
+</html>
